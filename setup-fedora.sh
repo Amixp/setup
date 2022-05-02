f0f5b13656d1413dd36b11203729ec576c86d24b
Artem <artemxp@gmail.com>
2022-05-02 12:01:42 +1000

    addd icecat browser

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -202,3 +202,19 @@ flatpak install -y flathub com.jetbrains.PhpStorm
 #Far file commander
 dnf copr enable polter/far2l -y
 dnf install far2l -y
+
+
+tee  /etc/yum.repos.d/copr.firefox-esr.repo << "EOF"
+[copr:copr.fedorainfracloud.org:beno:firefox-esr]
+name=Copr repo for firefox-esr owned by beno
+baseurl=https://download.copr.fedorainfracloud.org/results/beno/firefox-esr/fedora-34-$basearch/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://download.copr.fedorainfracloud.org/results/beno/firefox-esr/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1
+EOF
+
+dnf install firefox firefox-wayland profile-cleaner icecat -y
